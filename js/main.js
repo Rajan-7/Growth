c8ef5bf6b9bc21e1c8f8779183e48b459f8f9a2a
Rajan mabo limbu <rajanmabolimbu@kcc.edu.np>
2024-05-24 17:22:06 +0545

    ft:responsive

---
+++ js/main.js
@@ -0,0 +1,45 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const faqContainer = document.querySelector(".faq-content");
+
+  faqContainer.addEventListener("click", (e) => {
+    const groupHeader = e.target.closest(".faq-group-header");
+
+    if (!groupHeader) return;
+
+    const group = groupHeader.parentElement;
+    const icon = groupHeader.querySelector("i");
+    const groupBody = group.querySelector(".faq-group-body");
+
+    // toggle
+
+    icon.classlist.toggle("fa-plus");
+    icon.classlist.toggle("fa-minus");
+
+    // toggle the open of the body
+
+    groupBody.classlist.toggle("open");
+
+    // close other faq bodies
+    const otherGroups = document.querySelectorAll(".faq-group");
+
+    otherGroups.forEach((otherGroup) => {
+      if (otherGroup !== group) {
+        const otherGroupBody = otherGroup.querySelector(".faq-group-body");
+        const otherIcon = otherGroup.querySelector("i");
+
+        otherGroupBody.classList.remove("open");
+        otherIcon.classList.remove("fa-minus");
+        otherIcon.classList.add("fa-plus");
+      }
+    });
+  });
+});
+
+// mobile menu
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburgerButton = document.querySelectorAll(".hamburger-button");
+  const mobileMenu = document.querySelectorAll(".mobile-menu");
+  hamburgerButton.addEventListener("click", () => {
+    mobileMenu.classList.add(".active");
+  });
+});
